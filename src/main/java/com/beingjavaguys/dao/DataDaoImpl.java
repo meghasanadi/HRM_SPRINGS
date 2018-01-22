18cf9421b6d09202a27e9053536937df2c0045fc
meghasanadi <meghakf@yahoo.com>
2018-01-22 18:53:27 +0530

    springmvccrud

---
+++ src/main/java/com/beingjavaguys/dao/DataDaoImpl.java
@@ -0,0 +1,93 @@
+package com.beingjavaguys.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.beingjavaguys.model.Employee;
+
+public class DataDaoImpl implements DataDao {
+
+	@Autowired
+	SessionFactory sessionFactory;
+
+	Session session = null;
+	Transaction tx = null;
+
+	@Override
+	public boolean addEntity(Employee employee) throws Exception {
+
+		session = sessionFactory.openSession();
+		tx = session.beginTransaction();
+		session.save(employee);
+		tx.commit();
+		session.close();
+
+		return false;
+	}
+
+
+	@Override
+	public Employee getEntityById(long id) throws Exception {
+		session = sessionFactory.openSession();
+		Employee employee = (Employee) session.load(Employee.class,
+				new Long(id));
+		tx = session.getTransaction();
+		session.beginTransaction();
+		tx.commit();
+		return employee;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Employee> getEntityList() throws Exception {
+		session = sessionFactory.openSession();
+		tx = session.beginTransaction();
+		List<Employee> employeeList = session.createCriteria(Employee.class)
+				.list();
+		tx.commit();
+		session.close();
+		return employeeList;
+	}
+	
+	@Override
+	public boolean deleteEntity(long id)
+			throws Exception {
+		session = sessionFactory.openSession();
+		Object o = session.load(Employee.class, id);
+		tx = session.getTransaction();
+		session.beginTransaction();
+		session.delete(o);
+		tx.commit();
+		return false;
+	}
+
+
+	@Override
+	public boolean saveEmployee(Employee employee) throws Exception {
+		Session session=null;
+		boolean hasErrors=false;
+		try {
+			session=sessionFactory.openSession();
+			session.beginTransaction();
+			session.saveOrUpdate(employee);
+			session.getTransaction().commit();
+			
+		}catch(Exception e)
+		{
+			if(session!=null) {
+				session.getTransaction().rollback();
+			}
+			hasErrors=true;
+		}
+		finally {
+			if(session!=null) {
+				session.close();
+			}
+		}
+		return hasErrors;
+	}
+
+}
